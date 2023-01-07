9d553e7e9195b9c08863435065745cb88ce02595
glebanazarenko <106635217+glebanazarenko@users.noreply.github.com>
2023-01-08 02:51:56 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -3,20 +3,15 @@
     <head>
         <meta charset="utf-8" />
         <title>НормДом</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="Premium Bootstrap 5 Template" />
-        <meta name="keywords" content="bootstrap 5, premium, marketing, multipurpose" />
+        <meta name="keywords" content="НормДом" />
         <meta content="Mannatthemes" name="author" />
 
         <!-- favicon -->
-        <link rel="shortcut icon" href="img/NormDomLogoNOTEXT1.ico" />
+        <link rel="shortcut icon" href="images/NormDomLogoNOTEXT1.ico" />
 
         <!-- css -->
         <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <link href="css/style.min.css" rel="stylesheet" type="text/css" />
-
-        <!-- colors -->
-        <link href="css/colors/default.css" rel="stylesheet" type="text/css" id="color-opt" />
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
     </head>
 
 
@@ -312,5 +307,257 @@ fclose($file);
 */
 
 
-
+include "db.php";
 ?>
+
+    <body>
+        <!-- start navbar -->
+        <nav class="navbar navbar-expand-lg fixed-top sticky" id="navbar">
+            <div class="container">
+                <a href="index.html">
+                    <img src="images/NormDomTextFooter.png" alt="" height="50" />
+                </a><!--end navbar-brand-->
+
+                <div class="navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav mx-auto navbar-center mt-lg-0 mt-2">
+                        <li class="nav-item">
+                            <a class="nav-link-a active" href="index.php">Главная</a>
+                        </li><!--end nav-item-->
+                        <li class="nav-item">
+                            <a class="nav-link-a" href="main_black.php">Темная тема</a>
+                        </li><!--end nav-item-->
+                        <li class="nav-item">
+                            <a class="nav-link-a" href="services.html">Поиск</a>
+                        </li><!--end nav-item-->                        
+                        <li class="nav-item">
+                            <a class="nav-link-a" href="resume.html">Резюме</a>
+                        </li><!--end nav-item-->
+                        <li class="nav-item">
+                            <a class="nav-link-a" href="contact.php">Контакт</a>
+                        </li><!--end nav-item-->
+                    </ul><!--end navbar-nav-->
+                    <button type="button" class="btn btn-primary btn-hover" href="singUp.php">Регистрация</button>
+                    <button type="button" class="btn btn-green" href="singUp.php">Вход</button>
+                    <!--<a href="singUp.php" class="btn btn-sm nav-btn text-primary mb-4 mb-lg-0">Регистрация<i class="icon-xxs ms-1" data-feather="chevrons-right"></i></a>-->
+                </div><!-- end #navbarNav -->
+            </div><!-- end container -->
+        </nav>
+        <!-- end navbar -->
+
+        <!-- start hero -->
+        <section class="hero-one position-relative main-bg" id="home"  style="background-image: url(images/personal/main-bg.png); background-size: cover; background-position: center center;">
+            <div class="container">
+                <div class="row align-items-center justify-content-center">
+                    
+                    <div class="col-lg-5  mx-auto mt-5"> 
+                        <img src="images/personal/1.png" alt="" class="img-fluid ml-lg-5">
+                    </div><!--end col--> 
+                    <div class="col-lg-7 text-center px-0 px-xl-4 mt-5 mt-lg-0 pt-5 pt-lg-0">
+                        <h5 class="d-inline-block py-1 px-3 rounded text-muted font-secondary">Привет, Я Глеб Назаренко</h5>
+                        <h1 class="hero-title mb-4 font-secondary fo">Я фриланс <mark><span class="fw-medium typewrite" data-period="2000" data-type='["Java-", "PHP-", "Python-"]'></span></mark>разработчик</h1>   
+                        <span class="wrap"></span>                     
+                        <div class="mb-5 mb-lg-0">                           
+                            <div class="d-inline-block">
+                                <a href="resume.html" class="btn btn-primary">Резюме</a>
+                            </div>
+                        </div>
+                    </div><!--end col-->                  
+                </div><!--end row-->             
+            </div><!-- end container -->
+        </section>
+        <!-- end hero -->
+        <div class="position-relative">
+            <div class="shape overflow-hidden text-white">
+                <svg viewBox="0 0 2880 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0 48H1437.5H2880V0H2160C1442.5 52 720 0 720 0H0V48Z" fill="currentColor"></path>
+                </svg>
+            </div>
+        </div>
+        <!-- start about -->
+        <section class="section" id="about">
+            <div class="container">
+                <div class="row align-items-center">                
+                    <div class="col-lg-6 ms-auto align-self-center">
+                        <h5 class="fs-24 text-dark fw-medium mb-3"><mark>Персональные данные</mark></h5>       
+                        <h4 class="fw-normal lh-base text-gray-700 mb-5 fs-20">Давно установленный факт, что читатель будет отвлекаться на читаемое содержимое страницы и возьмет меня на работу.
+                        </h4>
+                        <div class="social mt-5">
+                            <a href="https://github.com/glebanazarenko">
+                                <i class="lab la-github github me-1"></i>
+                            </a>
+                            <a href="https://vk.com/hlebik_official">
+                                <i class="lab la-vk vk me-1"></i>
+                            </a>
+                            <a href="https://t.me/hlebik">
+                                <i class="lab la-telegram telegram me-1"></i>
+                            </a>
+                        </div>
+                    </div><!--end col-->
+
+                    
+                    <div class="col-lg-5 ms-auto align-self-center">
+                        <div class="mb-5 mb-lg-0">
+                            <p class="mb-2">
+                                <span class="personal-detail-title">Дата рождения</span> 
+                                <span class="personal-detail-info">26 июля 2003</span>
+                            </p>
+                            <p class="mb-2">
+                                <span class="personal-detail-title">Владение языками</span> 
+                                <span class="personal-detail-info">Русский - Английский - Программный</span>
+                            </p>
+                            <p class="mb-2">
+                                <span class="personal-detail-title">Национальность</span> 
+                                <span class="personal-detail-info">Русский</span>
+                            </p>
+                            <p class="mb-2">
+                                <span class="personal-detail-title">Интересы</span> 
+                                <span class="personal-detail-info">Чтение, Спорт, Сон</span>
+                            </p>
+                        </div>
+                    </div><!--end col-->
+                    </div><!--end row--> 
+            </div><!-- end container -->
+        </section>
+        <!-- end about -->
+
+        <section class="section" id="about">
+            <div class="container">
+                <div class="row align-items-center">                
+                    <div class="col-lg-6 ms-auto align-self-center">
+                        <h5 class="fs-24 text-dark fw-medium mb-3"><mark>Персональные данные</mark></h5>       
+                        <h4 class="fw-normal lh-base text-gray-700 mb-5 fs-20">Давно установленный факт, что читатель будет отвлекаться на читаемое содержимое страницы и возьмет меня на работу.
+                        </h4>
+                        <div class="social mt-5">
+                            <a href="https://github.com/glebanazarenko">
+                                <i class="lab la-github github me-1"></i>
+                            </a>
+                            <a href="https://vk.com/hlebik_official">
+                                <i class="lab la-vk vk me-1"></i>
+                            </a>
+                            <a href="https://t.me/hlebik">
+                                <i class="lab la-telegram telegram me-1"></i>
+                            </a>
+                        </div>
+                    </div><!--end col-->
+
+                    
+                    <div class="col-lg-5 ms-auto align-self-center">
+                        <div class="mb-5 mb-lg-0">
+                            <p class="mb-2">
+                                <span class="personal-detail-title">Дата рождения</span> 
+                                <span class="personal-detail-info">26 июля 2003</span>
+                            </p>
+                            <p class="mb-2">
+                                <span class="personal-detail-title">Владение языками</span> 
+                                <span class="personal-detail-info">Русский - Английский - Программный</span>
+                            </p>
+                            <p class="mb-2">
+                                <span class="personal-detail-title">Национальность</span> 
+                                <span class="personal-detail-info">Русский</span>
+                            </p>
+                            <p class="mb-2">
+                                <span class="personal-detail-title">Интересы</span> 
+                                <span class="personal-detail-info">Чтение, Спорт, Сон</span>
+                            </p>
+                        </div>
+                    </div><!--end col-->
+                    </div><!--end row--> 
+            </div><!-- end container -->
+        </section>
+
+        <section class="section" id="about">
+            <div class="container">
+                <div class="row align-items-center">                
+                    <div class="col-lg-6 ms-auto align-self-center">
+                        <h5 class="fs-24 text-dark fw-medium mb-3"><mark>Персональные данные</mark></h5>       
+                        <h4 class="fw-normal lh-base text-gray-700 mb-5 fs-20">Давно установленный факт, что читатель будет отвлекаться на читаемое содержимое страницы и возьмет меня на работу.
+                        </h4>
+                        <div class="social mt-5">
+                            <a href="https://github.com/glebanazarenko">
+                                <i class="lab la-github github me-1"></i>
+                            </a>
+                            <a href="https://vk.com/hlebik_official">
+                                <i class="lab la-vk vk me-1"></i>
+                            </a>
+                            <a href="https://t.me/hlebik">
+                                <i class="lab la-telegram telegram me-1"></i>
+                            </a>
+                        </div>
+                    </div><!--end col-->
+
+                    
+                    <div class="col-lg-5 ms-auto align-self-center">
+                        <div class="mb-5 mb-lg-0">
+                            <p class="mb-2">
+                                <span class="personal-detail-title">Дата рождения</span> 
+                                <span class="personal-detail-info">26 июля 2003</span>
+                            </p>
+                            <p class="mb-2">
+                                <span class="personal-detail-title">Владение языками</span> 
+                                <span class="personal-detail-info">Русский - Английский - Программный</span>
+                            </p>
+                            <p class="mb-2">
+                                <span class="personal-detail-title">Национальность</span> 
+                                <span class="personal-detail-info">Русский</span>
+                            </p>
+                            <p class="mb-2">
+                                <span class="personal-detail-title">Интересы</span> 
+                                <span class="personal-detail-info">Чтение, Спорт, Сон</span>
+                            </p>
+                        </div>
+                    </div><!--end col-->
+                    </div><!--end row--> 
+            </div><!-- end container -->
+        </section>
+
+        <section class="section" id="about">
+            <div class="container">
+                <div class="row align-items-center">                
+                    <div class="col-lg-6 ms-auto align-self-center">
+                        <h5 class="fs-24 text-dark fw-medium mb-3"><mark>Персональные данные</mark></h5>       
+                        <h4 class="fw-normal lh-base text-gray-700 mb-5 fs-20">Давно установленный факт, что читатель будет отвлекаться на читаемое содержимое страницы и возьмет меня на работу.
+                        </h4>
+                        <div class="social mt-5">
+                            <a href="https://github.com/glebanazarenko">
+                                <i class="lab la-github github me-1"></i>
+                            </a>
+                            <a href="https://vk.com/hlebik_official">
+                                <i class="lab la-vk vk me-1"></i>
+                            </a>
+                            <a href="https://t.me/hlebik">
+                                <i class="lab la-telegram telegram me-1"></i>
+                            </a>
+                        </div>
+                    </div><!--end col-->
+
+                    
+                    <div class="col-lg-5 ms-auto align-self-center">
+                        <div class="mb-5 mb-lg-0">
+                            <p class="mb-2">
+                                <span class="personal-detail-title">Дата рождения</span> 
+                                <span class="personal-detail-info">26 июля 2003</span>
+                            </p>
+                            <p class="mb-2">
+                                <span class="personal-detail-title">Владение языками</span> 
+                                <span class="personal-detail-info">Русский - Английский - Программный</span>
+                            </p>
+                            <p class="mb-2">
+                                <span class="personal-detail-title">Национальность</span> 
+                                <span class="personal-detail-info">Русский</span>
+                            </p>
+                            <p class="mb-2">
+                                <span class="personal-detail-title">Интересы</span> 
+                                <span class="personal-detail-info">Чтение, Спорт, Сон</span>
+                            </p>
+                        </div>
+                    </div><!--end col-->
+                    </div><!--end row--> 
+            </div><!-- end container -->
+        </section>
+
+        <?php
+        include "footer.php";
+        ?>
+        
+    </body>
+</html>
