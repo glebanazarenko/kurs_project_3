1b04c112a1202f86e948d32b02e60d3d5e3ee026
glebanazarenko <106635217+glebanazarenko@users.noreply.github.com>
2023-01-10 20:29:37 +0300

    Delete db.php

--- db.php
+++
@@ -1,8 +0,0 @@
-<?php
-    //--------------------------Настройки подключения к БД-----------------------
-    define('DB_HOST', 'localhost'); //Адрес
-    define('DB_USER', 'root'); //Имя пользователя
-    define('DB_PASSWORD', ''); //Пароль
-    define('DB_NAME', 'house'); //Имя БД
-    $mysql = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-?>
